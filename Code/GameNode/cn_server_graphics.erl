6dd3ae4a201c50b23be638da403feb1aaeb35511
Roi Alus <119787725+roialus@users.noreply.github.com>
2025-08-29 15:24:22 +0300

    Update cn_server_graphics.erl

--- Code/GameNode/cn_server_graphics.erl
+++ Code/GameNode/cn_server_graphics.erl
@@ -870,6 +870,7 @@ send_enhanced_map_to_gn_servers(State) ->
                     io:format("❌ Error sending data to GN server on ~w: ~p~n", [Node, Error])
             end;
         false ->
+                ok
             % io:format("⚠️ GN server on ~w not alive~n", [Node])
         end
     end, State#state.gn_graphics_servers).
