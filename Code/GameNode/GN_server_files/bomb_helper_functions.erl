71f62a4e6485d56b6a51a802b7f54b150f8816bb
Dolev <doleve46@gmail.com>
2025-09-04 21:35:15 +0300

    fix/debug: checking the correct return value for placing a bomb - inconsistent result with mnesia documentation

--- Code/GameNode/GN_server_files/bomb_helper_functions.erl
+++ Code/GameNode/GN_server_files/bomb_helper_functions.erl
@@ -33,7 +33,15 @@ place_bomb(PlayerNum, PlayersTableName, BombsTableName) ->
             case bomb_as_fsm:start_monitor(X, Y, BombType, [PlayerRecord#mnesia_players.pid, BombRadius]) of
                 {ok, {BombPid, _MonitorRef}} ->
                     %% Update player's active bomb within mnesia
-                    update_player_bomb_count(PlayerNum, PlayersTableName, 1),
+                    TestReturnVal = update_player_bomb_count(PlayerNum, PlayersTableName, 1), %% TODO: figure out which of these is correct
+                    io:format("DEBUG: update_player_bomb_count returned: ~p~n", [TestReturnVal]),
+                    case TestReturnVal of
+                        {atomic, _} -> ok;
+                        ok -> ok;
+                        Error1 -> 
+                            io:format("ERROR: update_player_bomb_count failed: ~p~n", [Error1]),
+                            {error, {failed_to_update_bomb_count, Error1}}
+                    end,
                     %% Generate bomb record for table
                     BombRecord = #mnesia_bombs{
                         position = [X,Y],
@@ -42,8 +50,19 @@ place_bomb(PlayerNum, PlayersTableName, BombsTableName) ->
                         owner = PlayerRecord#mnesia_players.player_number,
                         gn_pid = PlayerRecord#mnesia_players.target_gn,
                         pid = BombPid},
-                    {atomic, _Res} = add_bomb_to_table(BombRecord, BombsTableName),
-                    bomb_placed;
+                        TestReturnVal2 = add_bomb_to_table(BombRecord, BombsTableName),
+                        io:format("DEBUG: add_bomb_to_table returned: ~p~n", [TestReturnVal2]),
+                        case TestReturnVal2 of
+                            {atomic, _} -> 
+                                io:format("DEBUG: Bomb added to table successfully~n"),
+                                bomb_placed;
+                            ok -> 
+                                io:format("DEBUG: Bomb added to table (got ok instead of {atomic, ok})~n"),
+                                bomb_placed;
+                            Error2 -> 
+                                io:format("ERROR: add_bomb_to_table failed: ~p~n", [Error2]),
+                                {error, {failed_to_add_bomb_to_table, Error2}}
+                        end;
                 Error ->
                     {error, {failed_to_create_bomb, Error}}
             end;
