1569a42ee6abe1db79dc1232e984c33d7ddedd5a
Dolev <doleve46@gmail.com>
2025-09-05 23:41:00 +0300

    started implementing item-drop mechanism from a tile when breaking

--- Code/GameNode/GN_server_files/tile_helper_functions.erl
+++ Code/GameNode/GN_server_files/tile_helper_functions.erl
@@ -30,17 +30,37 @@
 %%% ===========================================================================
 break_tile(Position, TableName) ->
     Fun = fun() ->
-        ok = mnesia:delete(TableName, Position, write)
+        case mnesia:read(TableName, Position, sticky_write) of
+            [Tile = #mnesia_tiles{}] ->
+                % Check if tile contains an item and create it if needed
+                case Tile#mnesia_tiles.contains of
+                    undefined -> ok;
+                    empty -> ok;
+                    Item when Item =/= undefined, Item =/= empty ->
+                        % Create item at the tile's position
+                        erlang:send_after(1000, self(), {create_item, Position, Item})
+                        %TODO: this should send a self-message with a 1second delay or so to the GN server to spawn this item
+                end,
+                io:format("Tile at position ~p is being broken. Type: ~p~n", [Position, Tile#mnesia_tiles.type]),
+                ok = mnesia:delete(TableName, Position, write);
+            [] -> 
+                io:format("ERROR: break_tile: No tile found at position ~p in table ~p~n", [Position, TableName]),
+                not_found
+        end
     end,
-    mnesia:activity(transaction, Fun),
-    ok.
+    ReturnVal = case mnesia:activity(transaction, Fun) of
+        {atomic, R} -> R;
+        R -> R
+    end,
+    ReturnVal.
+
 
 update_to_one_hit(Position, TableName) ->
     Fun = fun() ->
         case mnesia:read(TableName, Position, sticky_write) of
             [Tile = #mnesia_tiles{}] ->
                 Updated_Tile = Tile#mnesia_tiles{type = one_hit},
-                mnesia:write(Updated_Tile),
+                mnesia:write(TableName, Updated_Tile, write),
                 ok;
             [] -> not_found
         end
